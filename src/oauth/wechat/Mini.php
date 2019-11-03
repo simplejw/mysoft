97526cdd77062e0c909d428dc72bdfeecccc113d
simplejw <xhwangjw@qq.com>
2019-11-03 21:47:21 +0800

    adjust

--- src/oauth/wechat/Mini.php
+++ src/oauth/wechat/Mini.php
@@ -6,7 +6,6 @@ use yii\base\Component;
 use yii\web\UnauthorizedHttpException;
 use yii\helpers\Json;
 
-use app\components\Helper;
 
 class Mini extends Component
 {
@@ -35,7 +34,7 @@ class Mini extends Component
 
             $url = $this->buildUrl(Wechat::ATURL, $params);
 
-			$token = Helper::curlContents($url);
+			$token = $this->curlContents($url);
             $result = Json::decode($token);
 
             if (is_array($result) && isset($result['errcode']))
@@ -51,27 +50,28 @@ class Mini extends Component
         return $result['access_token'];
     }
 
-    // code2Session get open_id and session_key
-    public function codeTwoSession($js_code = '')
+    /**
+     * auth.code2Session登陆, 获取 open_id and session_key
+     * @param string $jsCode
+     * @return array|mixed
+     * @throws UnauthorizedHttpException
+     */
+    public function codeTwoSession($jsCode = '')
     {
-        $params = [];
         $params['appid'] = $this->appID;
         $params['secret'] = $this->appSecret;
-        $params['js_code'] = $js_code;
+        $params['js_code'] = $jsCode;
         $params['grant_type'] = 'authorization_code';
 
         $url = $this->buildUrl(Mini::CSHOST, $params);
 
-        $token = Helper::curlContents($url);
-        $result = Json::decode($token);
+        $response = $this->curlContents($url);
+        $result = Json::decode($response);
 
         if (is_array($result) && isset($result['errcode']))
         {
             throw new UnauthorizedHttpException('wechat login code2Session failed ' . $result['errcode'] . ' ' . $result['errmsg']);
         }
-
-        // 自定义登录态
-        $result['login_key'] = md5($result['openid']);
 
         return $result;
     }
@@ -80,6 +80,43 @@ class Mini extends Component
 	{
 		if ($params) $url .= '?' . http_build_query($params, null, '&');
 		return $url;
+    }
+
+    /**
+     * 通过http请求数据
+     * @access public
+     * @param  string $url 网址
+     * @param  string $method 请求方式 默认GET
+     * @param  array  $args 请求参数
+     * @param  array  $header 头部
+     * @param  integer $timeout 超时时间 默认30秒
+     * @return string
+     */
+    public static function curlContents($url, $method = 'GET', $args = array(), $header = array(), $timeout = 30)
+    {
+        $response = '';
+
+        if (filter_var($url, FILTER_VALIDATE_URL))
+        {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+            curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36');
+
+            if ($method == 'POST')
+            {
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $args);
+            }
+
+            $response = curl_exec($ch);
+            curl_close($ch);
+        }
+
+        return $response;
     }
 
 }
